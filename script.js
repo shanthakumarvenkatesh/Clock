ed6bff7596395188094bef6fc240d667e4699caf
Shanthakumar <93003141+shanthakumarvenkatesh@users.noreply.github.com>
2022-03-30 13:07:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function clock(){
     var hrs=time.getHours();
     var mins=time.getMinutes();
     var secs=time.getSeconds();
-    var am_pm="PM";
+    var am_pm="AM";
     if(hrs==0){
         hrs=12;
     }
